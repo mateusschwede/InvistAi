e60e3cc3e2b81344f9e9f42e4c93cbc642bd8ff6
mateusschwede <mateus.schwede@hotmail.com>
2021-02-09 23:11:06 -0300

    Banco atualizado (Conforme detalhamento3)

--- banco.sql
+++ banco.sql
@@ -1,39 +1,53 @@
+/*Os nomes dos dados foram os mesmos informados pelo Tiago, para ñ confundirmos com as instruções do doc dele*/
+
 CREATE DATABASE invistai CHARSET=utf8;
 USE invistai;
 
 CREATE TABLE pessoa (
     cpf VARCHAR(11) NOT NULL, /*PK*/
-    nome VARCHAR(60) NOT NULL,
+    rg VARCHAR(10) NOT NULL,
+    nome VARCHAR(60) NOT NULL, /*Nome e Sobrenome*/
     email VARCHAR(60),
-    telefone VARCHAR(11),
+    celular VARCHAR(11),
+    endereco VARCHAR(200),
+    tipo INT NOT NULL, /*1-analista / 2-cliente*/
     senha VARCHAR(5) NOT NULL,
-    tipo INT NOT NULL DEFAULT 2, /*1-Analista, 2-Cliente*/
-    ativo BOOLEAN NOT NULL DEFAULT 1
+    inativado BOOLEAN DEFAULT 0
 );
 
 CREATE TABLE acao (
-    codigo VARCHAR(5) NOT NULL, /*PK*/
-    cnpj VARCHAR(12) NOT NULL,
+    ativo VARCHAR(5) NOT NULL, /*PK - Código dela, ex: OIBR4*/
+    cnpj VARCHAR(14) NOT NULL,
     nome VARCHAR(60) NOT NULL,
-    atividade VARCHAR(60) NOT NULL,
-    setor VARCHAR(60) NOT NULL,
-    preco FLOAT NOT NULL,
-    ativo BOOLEAN NOT NULL DEFAULT 1
+    atividade VARCHAR(80) NOT NULL,
+    setor VARCHAR(80) NOT NULL,
+    cotacaoAtual FLOAT NOT NULL, /*Preço da ação*/
+    inativado BOOLEAN NOT NULL DEFAULT 0
 );
 
 CREATE TABLE carteira (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(60) NOT NULL,
-    precoInvestimento FLOAT NOT NULL,
+    objetivo VARCHAR(60) NOT NULL, /*Ex: aposentadoria do bisneto, velório da sogra*/
+    investimento FLOAT NOT NULL, /*Valor investido*/
+    sobraAportes FLOAT NOT NULL DEFAULT 0 /*Resto do valor na divisão entre as ações*/
     cpfCliente VARCHAR(11) NOT NULL /*FK*/
 );
 
 CREATE TABLE carteira_acao (
     idCarteira INT NOT NULL, /*FK*/
-    codAcao VARCHAR(5) NOT NULL, /*FK*/
-    percentual INT NOT NULL,
-    precoPercentual FLOAT NOT NULL,
-    qtdAcoes INT,
-    nPercentual INT
+    ativoAcao VARCHAR(5) NOT NULL, /*FK código ação*/
+    qtdAcao INT NOT NULL, /*Qtd de ações compras de acordo com objetivo*/
+    patrAtualizado FLOAT NOT NULL, /*precoAção X qtdAcao*/
+    partAtual FLOAT NOT NULL, /*percent de participação atualizado de uma ação em relação a toda carteira (precoAção / patrAtualizado)*/
+    objetivo INT NOT NULL, /*percent definido pelo cliente para investimento da ação na carteira*/
+    distObjetivo FLOAT NOT NULL, /*percent de distância que falta para alcançar o objetivo*/
+    qntAcoesComprar FLOAT NOT NULL /*qtd de recomendação de compra de ações*/
 );
 
+CREATE TABLE movimentacao ( /*Table para representar as movimentações do cliente x ao analista (compras e vendas de ações)*/
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    dataMovimentacao DATETIME NOT NULL DEFAULT now(),
+    qtdAcoes INT NOT NULL, /*Compra: Valor positivo | Venda: Valor negativo*/
+    idCarteira INT NOT NULL, /*FK*/
+    ativoAcao VARCHAR(5) NOT NULL /*FK código ação*/
+);
