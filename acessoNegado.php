d3610df177be4d7c1302f30218a6c08440e3d4ca
Vinícius Kiefer <viniciuskiefer@outlook.com>
2021-02-16 00:15:24 -0300

    page acesso negado

---
+++ acessoNegado.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="https://img.icons8.com/fluent/96/000000/bad-idea.png">
+    <title>InvistAí - Acesso negado</title>
+    <link rel="stylesheet" href="estilo.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+<script src="../script.js"></script> 
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<div class="container">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <h1>InvistAí</h1>
+                <h2>Acesso negado!</h2>
+                <h4 class="text-muted">Favor fazer login para ter acesso.</h4>
+                <a href="index.php" class="btn btn-secondary btn-lg">Fazer login</a>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
