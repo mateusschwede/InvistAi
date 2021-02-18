462be63763dd21818a2871f76a7e1461ceeb66f8
mateusschwede <mateus.schwede@hotmail.com>
2021-02-18 17:51:53 -0300

    echo ultimaCotacao no index removido

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
     require_once 'conexao.php';
     $r = $db->query("SELECT dtUltimaCotacao FROM acao LIMIT 1");
-$linhas = $r->fetchAll(PDO::FETCH_ASSOC);
-foreach($linhas as $l) {$ultimaCotacaoBD = $l['dtUltimaCotacao'];}
-echo $ultimaCotacaoBD;
-if($ultimaCotacaoBD!=$ultimaCotacao) {
-    require_once 'api/dados.php';
-    require_once 'api/valores.php';
-}
+    $linhas = $r->fetchAll(PDO::FETCH_ASSOC);
+    foreach($linhas as $l) {$ultimaCotacaoBD = $l['dtUltimaCotacao'];}
+    if($ultimaCotacaoBD!=$ultimaCotacao) {
+        require_once 'api/dados.php';
+        require_once 'api/valores.php';
+    }
 
     if( (!empty($_POST['cpf'])) and (!empty($_POST['senha'])) ) {
         $r = $db->prepare("SELECT * FROM pessoa WHERE cpf=? AND senha=? AND inativado=0");
