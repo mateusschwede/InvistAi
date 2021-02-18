87061655f9286027651a4595c907acf7db892496
mateusschwede <mateus.schwede@hotmail.com>
2021-02-18 16:15:31 -0300

    Tabela listarAcoes inseridas

--- acoes.php
+++ acoes.php
@@ -2,19 +2,37 @@
 
 <div class="row">
     <div class="col-sm-12">
-        <h1>Ações</h1>
+    <h1>Ações</h1>
+    
+    <div class="table-responsive">
+        <table class='table table-striped'>
+            <thead>
+                <tr>
+                    <th scope='col'>Ativo</th>
+                    <th scope='col'>Nome</th>
+                    <th scope='col'>Setor</th>
+                    <th scope='col'>Cotação</th>
+                </tr>
+            </thead>
+            <tbody>
+
         <?php
             $r = $db->query("SELECT * FROM acao ORDER BY ativo");
             $linhas = $r->fetchAll(PDO::FETCH_ASSOC);
             foreach($linhas as $l) {
-                echo "
-                <p><b>Ativo:</b> ".strtoupper($l['ativo'])."</p>
-                <p><b>Nome:</b> ".$l['nome']."</p>
-                <p><b>Setor:</b> ".$l['setor']."</p>";
-                if($l['cotacaoAtual']!=0) {echo "<p><b>Cotação:</b> R$ ".number_format($l['cotacaoAtual'],2)."</p>";}
-                else {echo "<p class='text-muted'>Sem cotação disponível no momento</p>";}
-                echo "<hr>";
+            echo "
+                <tr>
+                    <th scope='row'>".strtoupper($l['ativo'])."</th>
+                    <td>".$l['nome']."</td>
+                    <td>".$l['setor']."</td>";
+                    if($l['cotacaoAtual']!=0) {echo "<td> R$ ".number_format($l['cotacaoAtual'],2)."</td>";}
+                    else {echo "<td class='text-muted'>Sem cotação disponível no momento</td>";}
+                echo "</tr>";
             }
         ?>
+            </tbody>
+        </table>
+    </div>
+
     </div>
 </div>
