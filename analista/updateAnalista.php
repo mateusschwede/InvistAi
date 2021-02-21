1b9b8ddad6a000d92e0006a4400b34c9deea788b
angelicaoliveira <38628166+angelicaoliveira@users.noreply.github.com>
2021-02-21 19:20:28 -0300

    Update Analista

--- analista/updateAnalista.php
+++ analista/updateAnalista.php
@@ -51,8 +51,27 @@
     <script type="text/javascript" src="../pace.min.js"></script>
 </head>
 <body>
-<div class="container">
     <div class="container-fluid">
+        <!-- Menu de Navegação -->
+        <div class="row">
+            <div class="col-sm-12" id="navbar">
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="index.php"><img src="https://img.icons8.com/fluent/24/000000/bad-idea.png"/> InvistAí<font size="2">(Analista)</font></a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                                <li class="nav-item"><a class="nav-link"href="perfil.php">Perfil</a></li>
+                                <li class="nav-item"><a class="nav-link active" aria-current="page" href="acoes.php">Ações</a></li>
+                                <li class="nav-item"><a class="nav-link" href="clientes.php">Clientes</a></li>
+                                  <li class="nav-item"><a class="nav-link" href="#" onclick="confirmlogout()" id="logout"><?=$_SESSION['nome']?>-logout</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
 
 
         <div class="row">
@@ -85,6 +104,5 @@
 
 
     </div>
-</div>
 </body>
 </html>
