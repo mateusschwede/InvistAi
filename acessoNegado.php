a71dfcdb969f357377a03811b677bb5bc9f49608
mateusschwede <mateus.schwede@hotmail.com>
2021-02-26 13:56:26 -0300

    Teste

--- acessoNegado.php
+++ acessoNegado.php
@@ -17,8 +17,7 @@
         <div class="row">
             <div class="col-sm-12 text-center">
                 <img src="https://img.icons8.com/fluent/44/000000/bad-idea.png">
-                <h1>InvistAí</h1>
-                <br>
+                <h1>InvistAí</h1><br>
                 <h2><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="bi bi-x-circle text-danger" viewBox="0 0 16 16"><path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg><br>Acesso negado!</h2>
                 <h4 class="text-muted">Favor fazer login para ter acesso.</h4>
                 <a href="index.php" class="btn btn-secondary btn-lg">Fazer login</a>
