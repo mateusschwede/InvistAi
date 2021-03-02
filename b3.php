e3d12c275c54aa5a02258d171200cbd762366d4f
“m19u31” <m19u31@outlook.com>
2021-03-02 18:04:38 -0300

    Update b3.php

--- b3.php
+++ b3.php
@@ -1,3 +1,6 @@
+<meta http-equiv="refresh" content="3600">
+<!-- essa é a página que atualiza o banco de dados usando api da b3. ela sera executada no servidor. 
+para simular esse comportamento no seu computador mantenha uma guia no navegador com essa janela aberta. -->
 <?php
     require_once 'conexao.php';
     require_once 'api/dados.php';
