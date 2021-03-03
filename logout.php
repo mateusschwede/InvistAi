d6b622f31907acf8ccf72f51870350e43772bc02
mateusschwede <mateus.schwede@hotmail.com>
2021-03-03 16:03:46 -0300

    logout.php otimizado

--- logout.php
+++ logout.php
@@ -1,8 +1,5 @@
 <?php
     session_start();
-    unset($_SESSION['cpf']);
-    unset($_SESSION['tipo']);
-    unset($_SESSION['nome']);
     session_unset();
     session_destroy();
     header("location: index.php");
