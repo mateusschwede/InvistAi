594860cc6808f44b2efc1618f1ee6fad4ee61bfd
mateusschwede <mateus.schwede@hotmail.com>
2021-03-07 20:11:45 -0300

    API bug corrigido e tds acoes inseridas

--- api/dados.php
+++ api/dados.php
@@ -1,53 +1,23 @@
 <?php
-    /*
-    $url1 = "https://api-cotacao-b3.labdo.it/api/sysinfo";
-    $ac = json_decode(file_get_contents($url1));
-    $ultimaCotacao = $ac->dt_ultimo_pregao;
+$url1 = "https://api-cotacao-b3.labdo.it/api/sysinfo";
+$ac = json_decode(file_get_contents($url1));
+$ultimaCotacao = $ac->dt_ultimo_pregao;
 
-    $r = $db->query("SELECT ativo FROM acao");
-    if($r->rowCount()==0) {
-        $url = "https://api-cotacao-b3.labdo.it/api/empresa";
-        $acao = json_decode(file_get_contents($url));
-        $c = 1;
+$r = $db->query("SELECT ativo FROM acao");
+if($r->rowCount()==0) {
+    $url = "https://api-cotacao-b3.labdo.it/api/empresa";
+    $acao = json_decode(file_get_contents($url));
 
-        foreach($acao as $a):
-            $ativo = explode(",",$a->cd_acao);
-            if($ativo[0]!="") {
-                $ativoBD = $ativo[0];
-                $nome = strtolower($a->nm_empresa);
-                $setor = mb_convert_case($a->segmento, MB_CASE_LOWER, "UTF-8");
+    foreach($acao as $a):
+        $ativo = explode(",",$a->cd_acao);
+        if($ativo[0]!="") {
+            $ativoBD = $ativo[0];
+            $nome = strtolower($a->nm_empresa);
+            $setor = mb_convert_case($a->segmento, MB_CASE_LOWER, "UTF-8");
 
-                $r = $db->prepare("INSERT INTO acao(ativo,nome,setor,dtUltimaCotacao) VALUES (?,?,?,?)");
-                $r->execute(array($ativoBD,$nome,$setor,$ultimaCotacao));
-            }
-        
-        endforeach;  
-    }*/
-
-
-
-
-    $url1 = "https://api-cotacao-b3.labdo.it/api/sysinfo";
-    $ac = json_decode(file_get_contents($url1));
-    $ultimaCotacao = $ac->dt_ultimo_pregao;
-
-    $r = $db->query("SELECT ativo FROM acao");
-    if($r->rowCount()==0) {
-        $url = "https://api-cotacao-b3.labdo.it/api/empresa";
-        $acao = json_decode(file_get_contents($url));
-        $c = 1;
-
-        foreach($acao as $a):
-            $ativo = explode(",",$a->cd_acao);
-            if($ativo[0]!="") {
-                $ativoBD = $ativo[0];
-                $nome = strtolower($a->nm_empresa);
-                $setor = mb_convert_case($a->segmento, MB_CASE_LOWER, "UTF-8");
-
-                $r = $db->prepare("INSERT INTO acao(ativo,nome,setor,dtUltimaCotacao) VALUES (?,?,?,?)");
-                $r->execute(array($ativoBD,$nome,$setor,$ultimaCotacao));
-            }
-            if($c==20){break;} else{$c++;}
-        endforeach;  
-    }
+            $r = $db->prepare("INSERT INTO acao(ativo,nome,setor,dtUltimaCotacao) VALUES (?,?,?,?)");
+            $r->execute(array($ativoBD,$nome,$setor,$ultimaCotacao));
+        }            
+    endforeach;  
+}
 ?>
