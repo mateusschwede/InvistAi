9bbeba1c305c78a70f334fa12c24fb3e45bee967
mateusschwede <mateus.schwede@hotmail.com>
2021-03-09 18:28:40 -0300

    AtualizarSenha (Links navbar desativados)

--- atualizarSenha.php
+++ atualizarSenha.php
@@ -63,15 +63,15 @@
             <div class="col-sm-12" id="navbar">
                 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="index.php"><img src="https://img.icons8.com/fluent/24/000000/bad-idea.png"/> InvistAí<font size="2">(Analista)</font></a>
+                        <a class="navbar-brand" href="#"><img src="https://img.icons8.com/fluent/24/000000/bad-idea.png"/> InvistAí</font></a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                         <div class="collapse navbar-collapse" id="navbarNav">
                             <ul class="navbar-nav">
-                                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-                                <li class="nav-item"><a class="nav-link"href="perfil.php">Perfil</a></li>
-                                <li class="nav-item"><a class="nav-link active" aria-current="page" href="acoes.php">Ações</a></li>
-                                <li class="nav-item"><a class="nav-link" href="clientes.php">Clientes</a></li>
-                                  <li class="nav-item"><a class="nav-link" href="#" onclick=" confirmlogout('../logout.php')" id="logout"><?=$_SESSION['nome']?>-logout</a></li>
+                                <li class="nav-item"><a class="nav-link disabled">Home</a></li>
+                                <li class="nav-item"><a class="nav-link disabled">Perfil</a></li>
+                                <li class="nav-item"><a class="nav-link disabled">Ações</a></li>
+                                <li class="nav-item"><a class="nav-link disabled">Clientes</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#" onclick=" confirmlogout('logout.php')" id="logout"><?=$_SESSION['nome']?>-logout</a></li>
                             </ul>
                         </div>
                     </div>
