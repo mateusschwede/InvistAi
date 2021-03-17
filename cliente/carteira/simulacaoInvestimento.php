b3e572f13853388109da63729f9af6bf598ecdfc
T4ylor <36934331+T4ylor@users.noreply.github.com>
2021-03-17 17:35:21 -0300

    Update simulacaoInvestimento.php

--- cliente/carteira/simulacaoInvestimento.php
+++ cliente/carteira/simulacaoInvestimento.php
@@ -371,7 +371,12 @@ if(isset($_GET['a'])){$a=$_GET['a'];$_SESSION['conf']=1;}else{$_SESSION['conf']=
     </div> <br><br><br>
 </body>
 <?php
-                     
+function acerto($u,$i){
+    for($x=1;$x<count($linhas)+2;$x++)
+                                        {
+            if($u/$i>=1){return 1;}
+    }                                            
+}         
    if($_SESSION['conf']==1)  // #########################################################
                             {
 
