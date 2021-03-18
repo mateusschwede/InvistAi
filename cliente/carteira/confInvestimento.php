873d6e73839fe0e282952c894897268bfbedeef4
T4ylor <36934331+T4ylor@users.noreply.github.com>
2021-03-18 10:58:29 -0300

    Update confInvestimento.php

--- cliente/carteira/confInvestimento.php
+++ cliente/carteira/confInvestimento.php
@@ -1,9 +1,8 @@
 <?php
     require_once '../../conexao.php';
   //  session_start();
+     unset($_SESSION['conf']);
     echo '<div class="alert alert-success">
-  <strong>Registrado!</strong> Lançamento em Operacao e soma nas quantidades da carteira_acao.
+  <strong>Registrado!</strong> <a href=" ../index.php?"class="btn btn-success" id="submitWithEnter" name="conf" style="text-align: center;" target="_top">Voltar</a>
 </div>';
-    unset($_SESSION['conf']);
-    include "../index.php";
 ?>
